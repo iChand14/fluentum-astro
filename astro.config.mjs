94f79df5aa96b8ca75f6b14761ad10915a10c1c6
iChand14 <satriachandra533@gmail.com>
2026-02-13 10:31:16 +0700

    Refactor:code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import path from 'path';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind()],
+    vite: {
+        resolve: {
+            alias: {
+                '@containers': path.resolve('./src/containers'),
+                '@components': path.resolve('./src/components'),
+                '@shared': path.resolve('./src/shared')
+            }
+        }
+    }
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
